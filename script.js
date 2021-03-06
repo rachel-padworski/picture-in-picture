094b20c3f59bfc22543845201d622a0af946657b
Rachel Padworski <rachelpadworski@gmail.com>
2021-03-06 10:05:09 -0700

    video in video

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('testing')
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button')
+
+// prompt user to select a media stream, then pass that to a video element, then play.
+async function selectMediaStream() {
+    try {
+        // Screen Capture API
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error) {
+        console.log('whoops, error here:', error)
+    }
+}
+
+// on load
+selectMediaStream();
